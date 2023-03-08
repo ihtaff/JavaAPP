b72806b5a6db057f9679c0824530e658410807a6
Fathi Mohssine <77497895+ihtaff@users.noreply.github.com>
2023-03-08 16:52:00 +0100

    Update ExtractDependencies.py

--- ExtractDependencies.py
+++ ExtractDependencies.py
@@ -1,5 +1,6 @@
 import json
 import xml.etree.ElementTree as ET
+from influxdb import InfluxDBClient
 
 # Chemin du fichier pom.xml
 pom_file = '/var/lib/jenkins/workspace/JavaProject/pom.xml'
@@ -18,6 +19,28 @@ for dependency in root.iter('dependency'):
     version = dependency.find('version').text.strip()
     dependencies[f"{group_id}:{artifact_id}"] = version
 
-# Écrit le dictionnaire de dépendances dans le fichier dependencies.json
-with open('dependencies.json', 'w') as f:
-    json.dump(dependencies, f, indent=2)
+# Crée une liste de points de données pour InfluxDB
+points = []
+for dep, version in dependencies.items():
+    point = {
+        "measurement": "dependencies",
+        "tags": {
+            "dependency": dep
+        },
+        "fields": {
+            "version": version
+        }
+    }
+    points.append(point)
+
+# Se connecte à la base de données InfluxDB
+client = InfluxDBClient(host='172.17.0.1', port=8090)
+
+# Crée une base de données "vulnerabilities" s'il n'existe pas
+db_name = 'vulnerabilities'
+if db_name not in client.get_list_database():
+    client.create_database(db_name)
+
+# Écrit les données de dépendances dans la base de données "vulnerabilities"
+client.switch_database(db_name)
+client.write_points(points)
